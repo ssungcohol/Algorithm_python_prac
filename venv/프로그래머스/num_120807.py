fcaedc558e859a6891dda32d801177e79d6e40b0
sungjae <sjhj825@naver.com>
2023-09-01 16:46:36 +0900

    09-01 숫자 비교하기_120807

---
+++ venv/프로그래머스/num_120807.py
@@ -0,0 +1,12 @@
+
+# 숫자 비교하기
+
+def solution (num1, num2) :
+    if num1== num2 :
+        print(1)
+    else : print(-1)
+
+    # answer = '1' if num1 == num2 else '-1'
+    # print(answer)
+
+solution(2, 3)
