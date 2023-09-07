b231e78db87687cf5e151f7d7560430d2ea4072d
sungjae <sjhj825@naver.com>
2023-09-07 17:12:31 +0900

    09-07 정수 내림차순으로 배치하기_12933

--- venv/프로그래머스/num_12933.py
+++ venv/프로그래머스/num_12933.py
@@ -2,17 +2,23 @@
 def solution(n):
     answer = 0
 
-    s = []
-    for i in str(n):
-        s.append(i)
+    # s = []
+    # for i in str(n):
+    #     s.append(i)
+    #
+    # s.sort(reverse=True)
+    #
+    # string = ''
+    # for i in s:
+    #     string += i
+    #
+    # answer = int(string)
+    # ====================================
 
-    s.sort(reverse=True)
-
-    string = ''
-    for i in s:
-        string += i
-
-    answer = int(string)
+    #한줄 간단화..
+    # s = str(n)
+    # s_list = sorted(s, reverse= True)
+    answer = int("".join(sorted(str(n), reverse= True)))
 
     print(answer)
 
