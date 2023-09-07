d81142dbc64827d760bc28c660780ae4a054a894
sungjae <sjhj825@naver.com>
2023-09-07 14:24:07 +0900

    09-07 옷가게 할인 받기_120818

---
+++ venv/프로그래머스/num_120818.py
@@ -0,0 +1,16 @@
+
+def solution(price):
+    answer = 0
+
+    if(price >= 500000) :
+        answer = int(price * 0.8)
+    elif(price >= 300000 and price < 500000):
+        answer = int(price * 0.9)
+    elif(price >= 100000 and price < 300000):
+        answer = int(price * 0.95)
+    else:
+        answer = int(price)
+
+    print(answer)
+
+solution(580000)
