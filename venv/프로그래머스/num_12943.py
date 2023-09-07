9f611030ca9be72441dd8a564befd20279187ea4
sungjae <sjhj825@naver.com>
2023-09-07 18:01:47 +0900

    09-07 콜라츠 추측_12943

---
+++ venv/프로그래머스/num_12943.py
@@ -0,0 +1,21 @@
+
+def solution(num):
+    answer = 0
+
+    while(num != 1):
+        if (num % 2 == 0):
+            num = num // 2
+            answer += 1
+            if(answer > 500):
+                answer = -1
+                break
+        elif (num % 2 != 0):
+            num = (num * 3) + 1
+            answer += 1
+            if(answer > 500):
+                answer = -1
+                break
+
+    print(answer)
+
+solution(626331)
