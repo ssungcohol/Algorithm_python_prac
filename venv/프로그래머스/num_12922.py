89d3195e7091ae516385f0ab6e6dc69de9799590
sungjae <sjhj825@naver.com>
2023-09-10 23:24:12 +0900

    09-10 수박수박수박수박수?_12922

---
+++ venv/프로그래머스/num_12922.py
@@ -0,0 +1,13 @@
+
+def solution(n):
+    answer = ''
+
+    for i in range(n):
+        if i % 2 == 0:
+            answer += '수'
+        else:
+            answer += '박'
+
+    print(answer)
+
+solution(3)
