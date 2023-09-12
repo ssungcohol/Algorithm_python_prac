b28e73bbd83065b832254ca3cb47be177d5ceb1c
sungjae <sjhj825@naver.com>
2023-09-12 16:07:21 +0900

    09-12 삼총사_131705

---
+++ venv/프로그래머스/num_131705.py
@@ -0,0 +1,13 @@
+
+def solution(number):
+    answer = 0
+
+    for i in range(len(number)):
+        for j in range(i+1, len(number)):
+            for k in range(j+1, len(number)):
+                if(number[i] + number[j] + number[k] == 0):
+                    answer += 1
+
+    print(answer)
+
+solution([-2, 3, 0, 2, -5])
