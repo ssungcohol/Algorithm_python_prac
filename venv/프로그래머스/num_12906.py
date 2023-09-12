8a5ef4a8d79449e5c06e01d8c3fdd0087c6138d9
sungjae <sjhj825@naver.com>
2023-09-12 15:12:15 +0900

    09-12 같은 숫자는 싫어_12906

--- venv/프로그래머스/num_12906.py
+++ venv/프로그래머스/num_12906.py
@@ -2,7 +2,7 @@
 def solution(arr):
     answer = []
 
-    for i in range(len(arr)):
+    for i    in range(len(arr)):
             if i == 0 or arr[i] != arr[i - 1]:
                 answer.append(arr[i])
 
