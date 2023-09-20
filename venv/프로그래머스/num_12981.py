6d8f9a942376b4173aa77f504f58d125c9c84d3b
sungjae <sjhj825@naver.com>
2023-09-20 18:42:17 +0900

    09-20 영어 끝말잇기_12981

---
+++ venv/프로그래머스/num_12981.py
@@ -0,0 +1,11 @@
+
+def solution(n, words):
+    answer = []
+
+    for i in range(1, len(words)):
+        if words[i][0] != words[i - 1][-1] or words[i] in words[:i] :
+            return [(i%n)+1, (i//n)+1]
+
+    return [0, 0]
+
+solution(5, ["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"])
