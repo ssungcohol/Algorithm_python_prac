4ca8fa0247100977a561557952d10fd4bf6a59f8
sungjae <sjhj825@naver.com>
2023-10-25 17:19:57 +0900

    10-25 가까운 1찾기_181898

---
+++ venv/프로그래머스/num_181898.py
@@ -0,0 +1,12 @@
+
+def solution(arr, idx):
+    answer = -1
+
+    for i in range(len(arr)):
+        if i >= idx and arr[i] == 1:
+            answer = i
+            break
+
+    print(answer)
+
+solution([0, 0, 0, 1], 1)
