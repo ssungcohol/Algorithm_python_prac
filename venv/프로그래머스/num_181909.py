d2ce3908cd7c7900fb5cee3b99ba005aacfcc103
sungjae <sjhj825@naver.com>
2023-10-29 11:26:26 +0900

    10-29 접미사 배열_181909

---
+++ venv/프로그래머스/num_181909.py
@@ -0,0 +1,12 @@
+
+def solution(my_string):
+    answer = []
+
+    for i in range(len(my_string)):
+        answer.append(my_string[i:])
+
+    answer.sort()
+
+    print(answer)
+
+solution("banana")
