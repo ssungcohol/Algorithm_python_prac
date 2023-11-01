42b83e00e7276ee918ff238c878f7e638856ca88
sungjae <sjhj825@naver.com>
2023-11-01 18:48:34 +0900

    11-01 수 조작하기 2_181925

---
+++ venv/프로그래머스/num_181925.py
@@ -0,0 +1,17 @@
+
+def solution(numLog):
+    answer = ''
+
+    for i in range(1, len(numLog)):
+        if numLog[i] - numLog[i-1] == 1:
+            answer += 'w'
+        elif numLog[i] - numLog[i-1] == -1:
+            answer += 's'
+        elif numLog[i] - numLog[i-1] == 10:
+            answer += 'd'
+        elif numLog[i] - numLog[i-1] == -10:
+            answer += 'a'
+
+    print(answer)
+
+solution([0, 1, 0, 10, 0, 1, 0, 10, 0, -1, -2, -1])
