678a627f68dccf4c312735a9b1508a26bba54d77
sungjae <sjhj825@naver.com>
2023-11-01 19:49:00 +0900

    11-01 문자열 섞기_181942

---
+++ venv/프로그래머스/num_181942.py
@@ -0,0 +1,11 @@
+
+def solution(str1, str2):
+    answer = ''
+
+    for i in range(len(str1)):
+        answer += str1[i]
+        answer += str2[i]
+
+    print(answer)
+
+solution("aaaaa", "bbbbb")
